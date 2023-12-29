f9dff8eaca94c9f71c8f21002379046d0aa2403e
matias-a-ruano <matias.a.ruano@gmail.com>
2023-12-29 14:10:57 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,9 @@
+const libros = [
+    { id: 1, titulo: 'Pinduli', autor: 'Janell Cannon' },
+    { id: 2, titulo: 'La Cabaña del Tio Tom', autor: 'H.B. Stowe' },
+    { id: 3, titulo: 'Asesinos Inocentes', autor: 'Jane Goodall' }
+];
+
+module.exports = libros;
+
+    
